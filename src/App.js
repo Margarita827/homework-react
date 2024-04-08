a115754ab798c6b2ed1441e3229440d892118566
Margarita827 <margaiba5@gmail.com>
2024-04-08 20:33:21 +0100

    add rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -7,23 +7,29 @@ export default function App() {
     <div className="App">
       <Weather />
       <footer>
-        Coded by 
-        <a href="https://github.com/Margarita827" target="_blank">
+        Coded by
+        <a
+          href="https://github.com/Margarita827"
+          target="_blank"
+          rel="noreferrer"
+        >
           Margarita Ibáñez
         </a>
-        , open sourced on 
+        , open sourced on
         <a
           href="https://github.com/Margarita827/homework-react"
           target="_blank"
+          rel="noreferrer"
         >
           GitHub
         </a>
-        , and website hosted on 
+        , and website hosted on
         <a
           href="https://app.netlify.com/sites/fluffy-blancmange-c8e322/overview"
           target="_blank"
+          rel="noreferrer"
         >
-        ,  Netlify
+          , Netlify
         </a>
       </footer>
     </div>
